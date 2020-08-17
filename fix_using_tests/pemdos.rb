84f58effee87076669a7447b07cbdb10963acc7f
Rodney Smith II <r_smith3@hotmail.com>
2020-08-17 20:41:56 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,11 @@
 # don't forget to add: require 'pry'
 
+require 'pry'
+
 def snake_it_up(string)
+  string = "surprise!"
   if string[0] == "s"
-    10 * "s" + string
+      string.rjust(19, "s")
   else
     string
   end
