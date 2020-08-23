7eee0ac8e1415dd1e1dcb03cbe36e15f1a70a247
Rodney Smith II <r_smith3@hotmail.com>
2020-08-23 23:43:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -7,11 +7,15 @@ def generate_star_date
 end
 
 
-def state_log(star_date)
+def engage(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
 def engage
   date = generate_star_date
+<<<<<<< HEAD
   puts state_log(date)
+=======
+  puts star_date
+>>>>>>> 6b19f7fdc4d57d24c5ad9fd9dde0e3b98c748e5e
 end
